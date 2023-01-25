12fb881e5dc4bb3bcc6d8d43879a04fc1c560b78
Onur Gungor Sahin <60578770+OnurSahin20@users.noreply.github.com>
2023-01-25 21:48:21 +0300

    Add files via upload

--- Sentinel3A_Download.py
+++ Sentinel3A_Download.py
@@ -102,38 +102,40 @@ class SentinelLST:
 
     def extract_zip(self):
         import zipfile
-        par_direcs = [self.down_path + "\\" + path for path in os.listdir(self.down_path)]  # folders to extract
+        par_direcs = [self.down_path + "\\Sentinel_LST_" + day for day in self.time_range[0:-1]]  # folders to extract
         print("unzipping is started !")
         if len(par_direcs) == 0:
             print("There is no zip file to extract.")
             return -1
         for par in par_direcs:
+            print(f"Zip files in {par} are extracting right now!")
             files = os.listdir(par)
             for z in files:
                 if ".zip" in z:
                     with zipfile.ZipFile(par + "\\" + z, "r") as zip_obj:
                         zip_obj.extractall(path=par)
 
-                pdirec = z.split(".")[0][-1]
-                fname = z.split(".")[0][0:-2] + ".SEN3"
-                os.rename(par + "\\" + fname, par + "\\" + fname + "_" + pdirec)
-            break
+                    pdirec = z.split(".")[0][-1]
+                    fname = z.split(".")[0][0:-2] + ".SEN3"
+                    os.rename(par + "\\" + fname, par + "\\" + fname + "_" + pdirec)
         print("Files are extracted successfully !")
 
 
 if __name__ == '__main__':
     us_name, pasword = "username", "password"  # authentication of sentinel open access hub.
-    download_loc = "download_loc"  # full path of download location of the LST products.
-    period = ("2022-01-01", "2022-02-01")  # [begin_date,end_date) ex.("2022-12-02", "2022-12-03") listed 1 day
-
-    shp_area = "shapefile location"
+    download_loc = "D:\\LST_Satellites\\Sentinel_3A"  # full path of download location of the LST products.
+    period = ("2023-01-01", "2023-01-11")  # [begin_date,end_date) ex.("2022-12-02", "2022-12-03") listed 1 day
+    # Rectangle of the study area (west,south,east,north)
+    """  Shape file implementation  
+    shp_area = "C:\\Users\\Gungor\\PycharmProjects\\sentinel_api\\WAB\\WAB_area.shp"
     gdf = geopandas.read_file(shp_area)
     w, s = gdf.bounds[["minx", "miny"]].min()
     e, n = gdf.bounds[["maxx", "maxy"]].max()
-    area = [w, s, e, n]  # Rectangle of the study area (west,south,east,north)
-    # It can be directly pass boundries of the study area without shapefile
-    # Spatial bounds also can be implemented using shapefile of study area (Later release!)
+    """
+
+    # Change area list depends on your area of interest (w,s,e,n) if there is no shapefile.
+    area = [26, 36, 30, 40]  # Rectangle of the study area (west,south,east,north)
     # time line can be "Non Time Critical" or "Near Real Time"
     lst = SentinelLST(download_loc, us_name, pasword, period[0], period[1], area, timeline="Non Time Critical")
-    # lst.download_all()
+    lst.download_all()
     lst.extract_zip()
