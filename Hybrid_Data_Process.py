34d9ea273376b8835bce0349105cbf0bceca9426
OnurSahin20 <ogungorsahin@gmail.com>
2023-02-04 19:35:33 +0300

    Hybridized Terra and SLSTR

--- Hybrid_Data_Process.py
+++ Hybrid_Data_Process.py
@@ -46,7 +46,7 @@ class HybridLst:
             if len(lst_df) == 0:
                 lst_data[f, :, :] = empty_data
             else:
-                x, y, model = sen_cla.re_griding(self.terra_lat, self.terra_lon,lst_df)
+                x, y, model = sen_cla.re_griding(self.terra_lat, self.terra_lon, lst_df)
                 lst_data[f, :, :] = model
         if len(product_filter) > 1:
             if np.all(np.isnan(lst_data[1, :, :])):
@@ -91,13 +91,13 @@ class HybridLst:
         t = ds.createVariable('time', 'f4', ('time',))
         x[:], y[:], t[:] = self.terra_lon, self.terra_lat, [_ for _ in range(len(self.time_period))]
         for product in ["terra", "slstr"]:
-            for d1, d2 in [("D", "day"), ("A", "night")]:
+            for d1 in ["day", "night"]:
                 if product == "terra":
-                    name = product + "_" + d2
+                    name = product + "_" + d1
                     values = ds.createVariable(name, 'f4', ("time", 'lat', 'lon',), fill_value=-9999)
-                    values[:, :, :] = self.build_time_series(product, d2)
+                    values[:, :, :] = self.build_time_series(product, d1)
                 else:
-                    name = product + "_" + d2
+                    name = product + "_" + d1
                     values = ds.createVariable(name, 'f4', ("time", 'lat', 'lon',), fill_value=-9999)
                     values[:, :, :] = self.build_time_series(product, d1)
         ds.close()
@@ -107,14 +107,14 @@ class HybridLst:
         d1 = self.get_sentinel_instant_lst(cur_date, date=date)
         d2 = self.get_terra_instant_lst(cur_date, date=date)
         if np.all(np.isnan(d1)):
-            vmin, vmax = np.nanmin(d2),np.nanmax(d2)
+            vmin, vmax = np.nanmin(d2), np.nanmax(d2)
         else:
             vmin, vmax = min(np.nanmin(d1), np.nanmin(d2)), max(np.nanmax(d1), np.nanmax(d2))
         fig, axs = plt.subplots(2, 2, figsize=(12, 8))
         fig1 = axs[0][0].pcolormesh(self.terra_lon, self.terra_lat, d1, vmin=vmin, vmax=vmax)
         axs[0][0].set_title(f"Sentinel LST for day {cur_date}"), fig.colorbar(fig1, ax=axs[0][0], label="LST (C)")
         fig2 = axs[0][1].pcolormesh(self.terra_lon, self.terra_lat, d2, vmin=vmin, vmax=vmax)
-        axs[0][1].set_title(f"MODIS Terra LST for day {cur_date}"),fig.colorbar(fig2, ax=axs[0][1], label="LST (C)")
+        axs[0][1].set_title(f"MODIS Terra LST for day {cur_date}"), fig.colorbar(fig2, ax=axs[0][1], label="LST (C)")
         fig3 = axs[1][0].pcolormesh(self.terra_lon, self.terra_lat, (d1 - d2))
         axs[1][0].set_title(f"Difference")
         fig.colorbar(fig3, ax=axs[1][0], label="LST Difference (C)")
